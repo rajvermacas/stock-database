4589f373f678028ee7caad85c78a073be2513b33
rajvermacas <141266274+rajvermacas@users.noreply.github.com>
2026-06-28 13:23:19 +0000

    Add 30m support

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,3 +1,4 @@
+uv run stock-data --config config/stock-data-30m.toml update-all
 uv run stock-data --config config/stock-data-1h.toml update-all
 uv run stock-data --config config/stock-data-1d.toml update-all
 uv run stock-data --config config/stock-data-1wk.toml update-all
